50bbecac4831c054f069178460b2805473687e43
Ephraim Mulilo <153711216+ephraimm-zm@users.noreply.github.com>
2025-10-20 16:40:14 +0200

    Load settings from localStorage and add saveSettings
    Updated settings to load from localStorage and added saveSettings function.

--- scripts/state.js
+++ scripts/state.js
@@ -2,11 +2,15 @@ import { load, save } from './storage.js';
 
 export let transactions = load();
 
-export let settings = {
+export let settings = JSON.parse(localStorage.getItem('finance:settings')) || {
   cap: 0,
   currency: 'ZMW'
 };
 
 export const updateState = () => {
   save(transactions);
+};
+
+export const saveSettings = () => {
+  localStorage.setItem('finance:settings', JSON.stringify(settings));
 };
